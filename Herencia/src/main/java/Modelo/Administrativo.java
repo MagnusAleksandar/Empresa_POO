aaff529aaf42d9a1e53a273cb25696f02ae8b40d
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 14:40:19 -0500

    Calcular completo

--- Herencia/src/main/java/Modelo/Administrativo.java
+++ Herencia/src/main/java/Modelo/Administrativo.java
@@ -1,35 +1,12 @@
 package Modelo;
 
 public class Administrativo extends Empleado {
-    private int descS, descP;
     private double prs;
     private double cuotLibranza;
     
-    public Administrativo(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, double prs) {
+    public Administrativo(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, double prs, double cuota) {
         super(id, nom, ape, tel, cc, sal, correo,t);
-    }
-
-    public int getDescS() {
-        return descS;
-    }
-
-    public void setDescS(int descS) {
-        this.descS = descS;
-    }
-
-    public int getDescP() {
-        return descP;
-    }
-
-    public void setDescP(int descP) {
-        this.descP = descP;
-    }
-
-    public double getCuotLibranza() {
-        return cuotLibranza;
-    }
-
-    public void setCuotLibranza(double cuotLibranza) {
-        this.cuotLibranza = cuotLibranza;
+        this.prs=prs;
+        this.cuotLibranza=cuota;
     }
 }
