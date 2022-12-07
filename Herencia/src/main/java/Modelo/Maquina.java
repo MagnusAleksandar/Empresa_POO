6f977616019b257d17bf82f047b1a42f3581b4e5
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 13:06:25 -0500

    Le agregué char

--- Herencia/src/main/java/Modelo/Maquina.java
+++ Herencia/src/main/java/Modelo/Maquina.java
@@ -2,8 +2,8 @@ package Modelo;
 
 public class Maquina extends Operario{
     
-    public Maquina(int id, String nom, String ape, double tel, double cc, double sal, String correo, int he, double st) {
-        super(id, nom, ape, tel, cc, sal, correo, he, st);
+    public Maquina(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, int he, double st) {
+        super(id, nom, ape, tel, cc, sal, correo, t, he, st);
     }
     
 }
