c0e04523a5838385ae0a3582382d9d6a39a691e4
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 13:05:23 -0500

    Le agregué char

--- Herencia/src/main/java/Modelo/Gerente.java
+++ Herencia/src/main/java/Modelo/Gerente.java
@@ -3,8 +3,8 @@ package Modelo;
 public class Gerente extends Administrativo{
     private String descr;
     
-    public Gerente(int id, String nom, String ape, double tel, double cc, double sal, String correo) {
-        super(id, nom, ape, tel, cc, sal, correo);
+    public Gerente(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t) {
+        super(id, nom, ape, tel, cc, sal, correo,t);
     }
 
     public String getDescr() {
