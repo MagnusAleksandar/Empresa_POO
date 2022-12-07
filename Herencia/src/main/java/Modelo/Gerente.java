d73616627666e3e1c46f0ebad58c5fb9100c5f3b
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 13:49:03 -0500

    Calcular completo

--- Herencia/src/main/java/Modelo/Gerente.java
+++ Herencia/src/main/java/Modelo/Gerente.java
@@ -2,9 +2,12 @@ package Modelo;
 
 public class Gerente extends Administrativo{
     private String descr;
+    private double prs;
     
-    public Gerente(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t) {
+    public Gerente(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, double prs) {
         super(id, nom, ape, tel, cc, sal, correo,t);
+        this.prs=prs;
+        
     }
 
     public String getDescr() {
