9dd68ee5f85c3466179426790a9453b785e405a9
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 13:04:55 -0500

    Le agregué char

--- Herencia/src/main/java/Modelo/Calcular.java
+++ Herencia/src/main/java/Modelo/Calcular.java
@@ -9,9 +9,31 @@ public class Calcular {
     PreServicio ps[];
     SGenerales sg[];
     Subgerente sub[];
+    Empleado em[];
     
-    public double nominaTotal(double salario){
-        double total=0;
+    public void pedirDatos(){
+        int id; 
+        String nom, ap, co;
+        double tel, cc, sal;
+        char t;
+        for(int i=0; i<em.length; i++){
+            if(i==0){
+            id=ob.pedirEntero("Inserte el ID de su empleado");
+            nom=ob.pedirString("Inserte el nombre de su empleado");
+            ap=ob.pedirString("Inserte el apellido de su empleado");
+            co=ob.pedirString("Inserte el correo de su empleado");
+            tel=ob.pedirDouble("Inserte el telefono de su empleado");
+            cc=ob.pedirDouble("Inserte la cedula de su empleado");
+            sal=ob.pedirDouble("Inserte el salario de su empleado");
+            t=ob.pedirChar("Inserte el tipo de empleado");
+            em[i]=new Empleado(id, nom, ap, tel, cc, sal, co, t);
+            }
+        }
+    }
+    
+    public double nominaTotal(){
+        double total=0, salario;
+        salario=0;
         total=salario+total;
         ob.mostrarRes("Lo que la mepresa paga a todos sus empleados en total es:"+total);
         return total;
