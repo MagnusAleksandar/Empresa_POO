7ebdd38e6aa61c7b87e1070f8c664694c1941a09
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-07 14:24:34 -0500

    Calcular completo

--- Herencia/src/main/java/Modelo/Maquina.java
+++ Herencia/src/main/java/Modelo/Maquina.java
@@ -2,7 +2,7 @@ package Modelo;
 
 public class Maquina extends Operario{
     
-    public Maquina(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, int he, double st) {
+    public Maquina(int id, String nom, String ape, double tel, double cc, double sal, String correo,char t, char he, double st) {
         super(id, nom, ape, tel, cc, sal, correo, t, he, st);
     }
     
