cec5cf8d5735e263b9a6c17315eeac5ef1502b98
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-06 22:36:46 -0500

    Wenas

--- Herencia/src/main/java/Modelo/Administrativo.java
+++ Herencia/src/main/java/Modelo/Administrativo.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author DJJORGEGUTIERREZ
- */
-public class Administrativo {
+public class Administrativo extends Empleado {
+    private int descS;
+    private int descP;
+    private double cuotLibranza;
     
+    public Administrativo(int id, String nom, String ape, double tel, double cc, double sal, String correo) {
+        super(id, nom, ape, tel, cc, sal, correo);
+    }
+
+    public int getDescS() {
+        return descS;
+    }
+
+    public void setDescS(int descS) {
+        this.descS = descS;
+    }
+
+    public int getDescP() {
+        return descP;
+    }
+
+    public void setDescP(int descP) {
+        this.descP = descP;
+    }
+
+    public double getCuotLibranza() {
+        return cuotLibranza;
+    }
+
+    public void setCuotLibranza(double cuotLibranza) {
+        this.cuotLibranza = cuotLibranza;
+    }
 }
