87ead10666c85a2dd437b23373fb45651b94da09
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-06 22:37:53 -0500

    Wenas

--- Herencia/src/main/java/Modelo/AtCliente.java
+++ Herencia/src/main/java/Modelo/AtCliente.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author DJJORGEGUTIERREZ
- */
-public class AtCliente {
+public class AtCliente extends Empleado {
+    private int descS;
+    private int descP;
     
+
+    public AtCliente(int id, String nom, String ape, double tel, double cc, double sal, String correo) {
+        super(id, nom, ape, tel, cc, sal, correo);
+    }
+
+    public int getDescS() {
+        return descS;
+    }
+
+    public int getDescP() {
+        return descP;
+    }
+
+    public void setDescS(int descS) {
+        this.descS = descS;
+    }
+
+    public void setDescP(int descP) {
+        this.descP = descP;
+    }    
 }
