e805c4b09ade181341a1973044f61642212e7dd0
alxev <alxev@DESKTOP-0CBK0T3>
2022-12-06 22:38:52 -0500

    Wenas

--- Herencia/src/main/java/Modelo/Subgerente.java
+++ Herencia/src/main/java/Modelo/Subgerente.java
@@ -9,5 +9,13 @@ package Modelo;
  * @author DJJORGEGUTIERREZ
  */
 public class Subgerente extends Administrativo{
+    private String descr;
     
+    public Subgerente(int id, String nom, String ape, double tel, double cc, double sal, String correo) {
+        super(id, nom, ape, tel, cc, sal, correo);
+    }
+
+    public String getDescr() {
+        return descr;
+    }
 }
